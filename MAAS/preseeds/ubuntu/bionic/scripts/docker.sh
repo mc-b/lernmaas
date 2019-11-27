105833087ae76a5a552c9e37e1bc3c7dd9ef0263
mc-b <marcel.bernet@ch-open.ch>
2019-11-27 16:24:59 +0100

    Grundinstallation

--- MAAS/preseeds/ubuntu/bionic/scripts/docker.sh
+++ MAAS/preseeds/ubuntu/bionic/scripts/docker.sh
@@ -8,3 +8,5 @@
 	wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb >/dev/null 2>&1
     sudo dpkg -i docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb
     rm -f docker-ce_18.06.1~ce~3-0~ubuntu_amd64.deb
+    
+    sudo usermod -aG docker ubuntu
