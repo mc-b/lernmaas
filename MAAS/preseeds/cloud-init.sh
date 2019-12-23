909f25ac0851fa7870a37b70e62f6e5c4cd33e69
mc-b <marcel.bernet@ch-open.ch>
2019-12-23 18:24:25 +0100

    TEst

--- MAAS/preseeds/cloud-init.sh
+++ MAAS/preseeds/cloud-init.sh
@@ -8,9 +8,9 @@ HOST=$(hostname | cut -d- -f 1)
 NO=$(hostname | cut -d- -f 2)
 MOD=$(grep "${HOST}=" config.txt | cut -d= -f2)
 
-## Module fuer Cloud-Init aufbereiten
+## Module fuer Cloud-Init aufbereiten - es muss alles in einer Datei stehen!
 
 for m in ${MOD}
 do
-    cp cloud.cfg.d/${m} /etc/cloud/cloud.cfg.d/
+    cat cloud.cfg.d/${m} >>/etc/cloud/cloud.cfg.d/99_lernmaas.cfg
 done 
