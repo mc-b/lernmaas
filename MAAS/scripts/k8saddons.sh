f2187e3698321122c244e930406ff467a8d827c5
mc-b <marcel.bernet@ch-open.ch>
2019-12-24 14:26:39 +0100

    k8s Standard

--- MAAS/scripts/k8saddons.sh
+++ MAAS/scripts/k8saddons.sh
@@ -5,14 +5,10 @@
 
 # Dashboard und User einrichten - Zugriff via kubectl proxy und Token mittels kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard | awk ' { print $1 }') Ermitteln
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
-kubectl apply -f /vagrant/addons/dashboard-admin.yaml
+kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/addons/dashboard-admin.yaml
 
 # Standard Persistent Volume und Claim
-kubectl create -f /vagrant/data/
+kubectl create -f https://raw.githubusercontent.com/mc-b/lernmaas/master/MAAS/data/DataVolume.yaml
 	
 # Weave Scope 
 kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
-# Kubeless
-kubectl create ns kubeless
-kubectl create -f https://github.com/kubeless/kubeless/releases/download/v1.0.0/kubeless-v1.0.0.yaml
--- MAAS/scripts/k8smaster.sh
+++ MAAS/scripts/k8smaster.sh
@@ -9,7 +9,7 @@ sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown 1000:1000 $HOME/.kube/config
+sudo chown ubuntu:ubuntu $HOME/.kube/config
 
 # this for loop waits until kubectl can access the api server that kubeadm has created
 for i in {1..150}; do # timeout for 5 minutes
@@ -26,8 +26,8 @@ kubectl taint nodes --all node-role.kubernetes.io/master-
 # Internes Pods Netzwerk (mit: --iface enp0s8, weil vagrant bei Hostonly Adapters gleiche IP vergibt)
 sysctl net.bridge.bridge-nf-call-iptables=1
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
-kubectl apply -f addons/kube-flannel.yaml
+kubectl apply -f https://raw.githubusercontent.com/mc-b/lernmaas/master/MAAS/addons/kube-flannel.yaml
 
 # Install ingress bare metal, https://kubernetes.github.io/ingress-nginx/deploy/
-kubectl apply -f addons/ingress-mandatory.yaml
-kubectl apply -f addons/service-nodeport.yaml
+kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/addons/ingress-mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/addons/service-nodeport.yaml
