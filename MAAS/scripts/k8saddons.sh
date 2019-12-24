084d398c3566d7e4ddda51c0338733b17cbb4cb1
mc-b <marcel.bernet@ch-open.ch>
2019-12-24 22:52:34 +0100

    Test

--- MAAS/scripts/k8saddons.sh
+++ MAAS/scripts/k8saddons.sh
@@ -4,11 +4,11 @@
 #
 
 # Dashboard und User einrichten - Zugriff via kubectl proxy und Token mittels kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard | awk ' { print $1 }') Ermitteln
-sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
-sudo kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/addons/dashboard-admin.yaml
+sudo su - ubuntu -c "kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml"
+sudo su - ubuntu -c "kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/addons/dashboard-admin.yaml"
 
 # Standard Persistent Volume und Claim
-sudo kubectl apply -f https://raw.githubusercontent.com/mc-b/lernmaas/master/MAAS/data/DataVolume.yaml
+sudo su - ubuntu -c "kubectl apply -f https://raw.githubusercontent.com/mc-b/lernmaas/master/MAAS/data/DataVolume.yaml"
 	
 # Weave Scope 
-sudo kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+sudo su - ubuntu -c "kubectl apply -f 'https://cloud.weave.works/k8s/scope.yaml?k8s-version='$(kubectl version | base64 | tr -d '\n')"
--- MAAS/scripts/k8smaster.sh
+++ MAAS/scripts/k8smaster.sh
@@ -20,12 +20,12 @@ sudo chown -R ubuntu:ubuntu /home/vagrant
 sleep 120
 
 # Pods auf Master Node erlauben
-sudo kubectl taint nodes --all node-role.kubernetes.io/master-
+sudo su - ubuntu -c "kubectl taint nodes --all node-role.kubernetes.io/master-"
 
 # Internes Pods Netzwerk (mit: --iface enp0s8, weil vagrant bei Hostonly Adapters gleiche IP vergibt)
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
-sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
+sudo su - ubuntu -c "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml"
 
 # Install ingress bare metal, https://kubernetes.github.io/ingress-nginx/deploy/
-sudo kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/addons/ingress-mandatory.yaml
-sudo kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/addons/service-nodeport.yaml
+sudo su - ubuntu -c "kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/addons/ingress-mandatory.yaml"
+sudo su - ubuntu -c "kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/addons/service-nodeport.yaml"
