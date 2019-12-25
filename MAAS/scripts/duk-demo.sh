9874cde142f22b59da17e74cb896c21e3a4a6556
mc-b <marcel.bernet@ch-open.ch>
2019-12-25 16:29:57 +0100

    Duk-demo

---
+++ MAAS/scripts/duk-demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#   Repository https://github.com/mc-b/duk-demo - Docker und Kubernetes Demoumgebung hinzufuegen
+#
+
+git clone https://github.com/mc-b/duk-demo
+
+cd duk-demo
+bash -x scripts/install.sh
