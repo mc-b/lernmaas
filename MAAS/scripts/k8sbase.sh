a1346c28e0b6db241dcb2eb8f4949fd3b2ee385c
mc-b <marcel.bernet@ch-open.ch>
2019-12-25 10:20:44 +0100

    Update apt Repository

--- MAAS/scripts/k8sbase.sh
+++ MAAS/scripts/k8sbase.sh
@@ -5,6 +5,6 @@
 VERSION=1.16.1-00
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/Kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/Kubernetes.list
 sudo apt-get -q 2 update
 sudo apt-get install -q 2 -y kubelet=${VERSION} kubeadm=${VERSION}
