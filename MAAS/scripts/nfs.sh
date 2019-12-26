2e3f532d659da7cf30d5fee70714b10ecadd187f
mc-b <marcel.bernet@ch-open.ch>
2019-12-26 16:11:24 +0100

    Nur eigene Join Datei loeschen

--- MAAS/scripts/nfs.sh
+++ MAAS/scripts/nfs.sh
@@ -28,8 +28,8 @@ sudo mkdir -p /home/ubuntu/data/${HOSTNAME} && chown ubuntu:ubuntu /home/ubuntu/
 sudo umount /home/ubuntu/data
 sudo mount -t nfs ${SERVER_IP}:/data/storage/${HOSTNAME} /home/ubuntu/data
 
-# CleanUp alte Join Dateien von Kubernetes
-sudo rm -f /home/ubuntu/data/join-*.sh
+# CleanUp alte Join Datei von Kubernetes
+sudo rm -f /home/ubuntu/data/join-$(hostname).sh
 
 # update /etc/fstab for reboots
 cat <<%EOF% | sudo tee -a /etc/fstab
