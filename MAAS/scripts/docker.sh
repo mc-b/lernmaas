311c5d17bf55041cab9293df6dd645f244aac591
mc-b <marcel.bernet@ch-open.ch>
2019-12-28 19:57:52 +0100

    k8s Cache beruecksichtigen

--- MAAS/scripts/docker.sh
+++ MAAS/scripts/docker.sh
@@ -14,6 +14,16 @@ sudo usermod -aG docker ubuntu
 # Worker Node - Hostname vom Master verwenden
 HOST=$(hostname | cut -d- -f 1 | sed -e 's/worker/master/g')
 
+# Kubernetes Images 
+if  [ -d /home/ubuntu/templates/cr-cache/k8s ]
+then
+    for image in /home/ubuntu/templates/cr-cache/k8s/*.tar
+    do
+        sudo docker load -i ${image}
+    done
+fi
+
+# Modul spezifische Images
 if  [ -d /home/ubuntu/templates/cr-cache/${HOST} ]
 then
     for image in /home/ubuntu/templates/cr-cache/${HOST}/*.tar
