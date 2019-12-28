e47c68f4b99091d356f9cc8ce8476cc1b96524a8
mc-b <marcel.bernet@ch-open.ch>
2019-12-28 09:46:11 +0100

    Verzeichnis

--- MAAS/scripts/docker.sh
+++ MAAS/scripts/docker.sh
@@ -16,7 +16,7 @@ HOST=$(hostname | cut -d- -f 1 | sed -e 's/worker/master/g')
 
 if  [ -d /home/ubuntu/templates/cr-cache/${HOST} ]
 then
-    for image in /data/templates/cr-cache/${HOST}/*.tar
+    for image in /home/ubuntu/templates/cr-cache/${HOST}/*.tar
     do
         sudo docker load -i ${image}
     done
