62a5c34cac0e907287f5e9cf81d5e3371e3af146
mc-b <marcel.bernet@ch-open.ch>
2019-12-30 18:13:40 +0100

    SSH Key generieren

--- services/sshkeys.sh
+++ services/sshkeys.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 #
-#   SSH Key hinterlegen
+#   SSH Key hinterlegen bzw. generieren
 #
+
+# eigenen SSH Key fuer VM generieren und Privaten Schluessel im Data Verzeichnis verfuegbar machen
+if  [ "$1" == "generate" ]
+then
+    cd $HOME
+    ssh-keygen -t rsa -f .ssh/id_rsa -b 4096 -P ''
+    cat .ssh/id_rsa.pub >>.ssh/authorized_keys
+    mkdir -p data/.ssh
+    chmod 700 data/.ssh
+    cp .ssh/id_rsa data/.ssh
+fi    
