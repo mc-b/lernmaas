7e84c25bd1a16826d815acb920ed464d2ce4183c
mc-b <marcel.bernet@ch-open.ch>
2020-01-15 08:21:46 +0100

    Docker pull raus - verwende cr-cache

--- scripts/modtec.sh
+++ scripts/modtec.sh
@@ -4,8 +4,8 @@
 #
 
 # IoT Umgebung (werden als Teil einer Uebung gestartet)
-# kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/iot/mosquitto.yaml
-# kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/iot/nodered.yaml
+kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/iot/mosquitto.yaml
+kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/iot/nodered.yaml
 
 # Messaging Umgebung (MQTT - Kafka Bridget neu mit Node-RED)
 kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/kafka/zookeeper.yaml
@@ -23,7 +23,7 @@ sudo chmod 755 $HOME/data/.ssh
 chmod 644 $HOME/data/.ssh/id_rsa $HOME/data/.ssh/id_rsa.ppk
 
 # BPMN Umgebung und Upload BPMN Prozess
-sudo docker pull camunda/camunda-bpm-platform
+# sudo docker pull camunda/camunda-bpm-platform
 kubectl apply -f https://raw.githubusercontent.com/mc-b/misegr/master/bpmn/camunda.yaml
 
 cd $HOME
