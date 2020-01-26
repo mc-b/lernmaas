fc0be7be9adb3053c8bbed4f98b4778d1f80bf12
mc-b <marcel.bernet@ch-open.ch>
2020-01-26 17:55:50 +0100

    Rancher

--- services/rancher.sh
+++ services/rancher.sh
@@ -2,7 +2,8 @@
 #
 #   Kubernetes Rancher Installation
 #
-
+cd $HOME
 git clone https://github.com/rancher/quickstart.git
 cd quickstart/vagrant
+
 sudo bash -x scripts/configure_rancher_server.sh admin v2.2.10 ""
