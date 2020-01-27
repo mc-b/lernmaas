115a32503aa94475f7de373da79bc69cc4a20136
mc-b <marcel.bernet@ch-open.ch>
2020-01-27 08:22:39 +0100

    Hinweis Vagrantfile

--- services/rancher.sh
+++ services/rancher.sh
@@ -2,4 +2,7 @@
 #
 #   Kubernetes Rancher Installation
 #
+#   Installiert nur das UI um Cluster zu erstellen.
+#   TODO: https://github.com/rancher/quickstart integrieren 
+#
 sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
