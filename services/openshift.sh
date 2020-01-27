d82b419f1d1f7b208c2310f07a842070d72520d3
mc-b <marcel.bernet@ch-open.ch>
2020-01-27 11:01:30 +0100

    OpenShift

--- services/openshift.sh
+++ services/openshift.sh
@@ -21,8 +21,8 @@ EOF
 sudo systemctl restart docker
 
 # OpenShift starten (Hostname = WireGuard IP)
-oc cluster up --public-hostname=$(hostname -I | cut -d" " -f2)
+sudo oc cluster up --public-hostname=$(hostname -I | cut -d" " -f2)
 
 # Restart weil redirect auf 127.0.0.1 aktiv
-oc cluster down
-oc cluster up --public-hostname=$(hostname -I | cut -d" " -f2)
+sudo oc cluster down
+sudo oc cluster up --public-hostname=$(hostname -I | cut -d" " -f2)
