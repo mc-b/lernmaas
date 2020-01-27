1e3f55a060fbebb0de10988f8526a7fb28bfa763
mc-b <marcel.bernet@ch-open.ch>
2020-01-27 09:53:04 +0100

    OpenShift

--- services/openshift.sh
+++ services/openshift.sh
@@ -7,8 +7,8 @@
 cd $HOME
 
 wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-tar xvzf openshift*
-cd openshift
+tar xvzf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+cd openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit
 
 sudo mv oc kubectl /usr/local/bin
 
