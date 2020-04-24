d6e81a53080c7723dc655d2ee8c5cd073dd7b373
mc-b <marcel.bernet@ch-open.ch>
2020-04-24 10:30:50 +0200

    k3s ohne ingress controller

--- services/k3s.sh
+++ services/k3s.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #   
-#   Installiert Rancher k3s
+#   Installiert Rancher k3s ohne Ingress Controller traefik
 #
 
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy=traefik" sh -s -
 
 # ubuntu User als Admin zulassen
 sudo mkdir -p /home/ubuntu/.kube
@@ -12,16 +12,7 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 sudo chmod 700 /home/ubuntu/.kube
 sudo echo 'export KUBECONFIG=$HOME/.kube/config' >>/home/ubuntu/.bashrc 
 
-# Persistent Volumes und Claims einrichten - Daten werden auf MAAS Server /data/storage/$(hostname) gespeichert
+# TODO Persistent Volumes und Claims einrichten - Daten werden auf MAAS Server /data/storage/$(hostname) gespeichert
 
-# Verzeichnis fuer Persistent Volume
-if [ -d $HOME/data ]
-then 
-    sudo ln -s $HOME/data /data
-else
-    sudo mkdir /data
-    sudo chown ubuntu:ubuntu /data
-    sudo chmod 777 /data
-fi 
 
-sudo kubectl apply -f https://raw.githubusercontent.com/mc-b/lernkube/master/data/DataVolume.yaml
+
