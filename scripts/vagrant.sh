7c8d76fb98796e90887d247c5f3916bb31dc1b0f
mc-b <marcel.bernet@ch-open.ch>
2020-08-17 11:33:38 +0200

    Neue Scripts fuer Vagrant und Eclipse Theia IDE, Mehr Power fuer M300

---
+++ scripts/vagrant.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#   Installiert Vagrant und VirtualBox z.B. fuer Modul M300.
+#
+#   ACHTUNG: damit Vagrant und VBox funktioniert muss VM in VM enabled (siehe helper/enablevminvm) sein.
+#
+
+sudo apt-get install -y vagrant virtualbox
