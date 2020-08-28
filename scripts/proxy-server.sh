a885639484b58b0d30d8552e0edfe8fc059e0232
mc-b <marcel.bernet@ch-open.ch>
2020-08-28 09:26:59 +0200

    Proxy Server und ngrok

---
+++ scripts/proxy-server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#   Einrichten des Proxy Servers Privoxy - https://www.privoxy.org/ und ngrok Tunnels https://ngrok.com/
+#
+#   Windows PC einrichten
+#   - Proxyeinstellungen
+#   - IP-Adresse des Servers und Port 8118 angeben
+
+# Proxy Server
+sudo apt-get install -y privoxy
+
+# wenn WireGuard installiert - Wireguard IP als Proxy IP
+export ADDR=$(ip -f inet addr show wg0 | grep -Po 'inet \K[\d.]+')
+[ "${ADDR}" = "" ] && { export ADDR=$(hostname -I | cut -d ' ' -f 1); }
+sudo sed -i -e "s/127.0.0.1:8118/${ADDR}:8118/g" /etc/privoxy/config
+
+sudo systemctl restart privoxy
+
+# ngrok Tunnels
+sudo snap install ngrok
