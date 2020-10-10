fcd39348d5e851f78865e0d335ecb1448a666cb8
mc-b <marcel.bernet@ch-open.ch>
2020-10-10 18:55:19 +0200

    Helm und Metrics Server mit Dashboard installieren

--- services/k8sbase.sh
+++ services/k8sbase.sh
@@ -14,3 +14,8 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/Kubernetes.list
 sudo apt-get -q 2 update
 sudo apt-get install -q 2 -y kubelet=${VERSION} kubeadm=${VERSION}
+
+##########################
+# Package Manager (HELM - Achtung bei Versionwechsel auch client.sh aendern).
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
