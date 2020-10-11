b612ccc16cb357b178e96f08f8b1323ff400fd50
mc-b <marcel.bernet@ch-open.ch>
2020-10-11 17:48:10 +0200

    Test

--- services/k8sbase.sh
+++ services/k8sbase.sh
@@ -14,3 +14,15 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/Kubernetes.list
 sudo apt-get -q 2 update
 sudo apt-get install -q 2 -y kubelet=${VERSION} kubeadm=${VERSION}
+
+# Bug vagrant, @see https://linuxacademy.com/community/posts/show/topic/29447-pod-is-not-found-eventhough-pod-status-is-up-and-running-why
+if  [[ "$(hostname)" =~ "worker" ]]
+then
+    cat <<%EOF% | sudo tee -a /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+Environment="KUBELET_EXTRA_ARGS=--node-ip=$(hostname -I | cut '-d ' -f2)"
+%EOF%
+
+    sudo systemctl daemon-reload
+    sudo systemctl restart kubelet
+
+fi
