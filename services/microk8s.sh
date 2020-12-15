e4a189f1bc06ce087043ab09d3a1326d1823c2f0
mc-b <marcel.bernet@ch-open.ch>
2020-12-15 17:51:25 +0100

    Automatischer Join analog Standard Kubernetes

--- services/microk8s.sh
+++ services/microk8s.sh
@@ -11,10 +11,40 @@ sudo chown -f -R ubuntu ~/.kube
 sudo echo 'alias kubectl="microk8s kubectl"' >>/home/ubuntu/.bashrc 
 sudo microk8s enable dns storage
 
+SERVER_IP=$(sudo cat /var/lib/cloud/instance/datasource | cut -d: -f3 | cut -d/ -f3)
+MASTER=$(hostname | cut -d- -f 3,4)
+
+# Master vorhanden?
+if  [ "${SERVER_IP}" != "" ] && [ "${MASTER}" != "" ]
+then
+
+    # Master statt Worker Node mounten
+    sudo umount /home/ubuntu/data
+    sudo mount -t nfs ${SERVER_IP}:/data/storage/${MASTER} /home/ubuntu/data/
+    sudo sed -i -e "s/$(hostname)/${MASTER}/g" /etc/fstab
+    
+    # Password und ssh-key wie Master
+    sudo chpasswd <<<ubuntu:$(cat /home/ubuntu/data/.ssh/passwd)
+    cat /home/ubuntu/data/.ssh/id_rsa.pub >>/home/ubuntu/.ssh/authorized_keys
+    
+    # loop bis Master bereit, Timeout zwei Minuten
+    for i in {1..60}
+    do
+        if  [ -f /home/ubuntu/data/.ssh/id_rsa ]
+        then
+            sudo chmod 600 /home/ubuntu/data/.ssh/id_rsa
+            sudo $(ssh -i /home/ubuntu/data/.ssh/id_rsa ${MASTER} microk8s add-node | awk 'NR==2 { print $0 }')
+            sudo chmod 666 /home/ubuntu/data/.ssh/id_rsa
+            break
+        fi
+        sleep 2
+    done
+fi
+
 # Intro
-
+    
 cat <<%EOF% | sudo tee README.md
-
+    
 ### microk8s Kubernetes
 
 [![](https://img.youtube.com/vi/v9KI2BAF5QU/0.jpg)](https://www.youtube.com/watch?v=v9KI2BAF5QU)
