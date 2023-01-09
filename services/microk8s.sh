131106cbc17b5b5e8c74f3b79741de49e8e5158b
mc-b <marcel.bernet@ch-open.ch>
2023-01-09 09:27:41 +0100

    Ubuntu 20/22 Abhandlung

--- services/microk8s.sh
+++ services/microk8s.sh
@@ -53,12 +53,21 @@ sudo chown -f -R ubuntu:ubuntu /home/ubuntu/.kube
 
 ###
 # containers Tools Installieren
-sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_18.04/Release.key -O /tmp/Release.key
-sudo apt-key add - </tmp/Release.key
-sudo apt-get update -qq
-sudo apt-get -qq -y install podman buildah skopeo  
-sudo apt-get -qq -y install fuse-overlayfs
+source /etc/os-release
+# Ubuntu 20
+if [ "${VERSION_CODENAME}" == "focal" ]
+then
+    sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+    wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_20.04/Release.key -O /tmp/Release.key
+    sudo apt-key add - </tmp/Release.key
+    sudo apt-get update -qq
+    sudo apt-get -qq -y install podman buildah skopeo  
+    sudo apt-get -qq -y install fuse-overlayfs
+# Containers Tools (ab Ubuntu 22.x)
+else
+    sudo apt-get update -qq
+    sudo apt-get install -y podman buildah skopeo
+fi    
 # Enable remote access @see https://www.redhat.com/sysadmin/podman-clients-macos-windows
 systemctl --user enable podman.socket
 sudo loginctl enable-linger $USER
